9539c56a743cd01f4a9eb539352b8eac51f87b21
Piotr Skibinski <pskib00@gmail.com>
2021-09-26 22:53:06 +0200

    MEP Live View 1.0: SocketIO setup.

--- tools/server.py
+++ tools/server.py
@@ -1,33 +1,33 @@
 from flask import Flask, flash, redirect, render_template, request, session, url_for, jsonify
+from flask_socketio import SocketIO, send
+from apscheduler.schedulers.background import BackgroundScheduler
 import sqlite3
 from time import sleep
 app = Flask(__name__)
+socketio = SocketIO(app)
 
-@app.route("/", methods=["GET"])
+static = 10;
+@app.route("/")
 def index():
-    return render_template("/main-page.html")
+    return render_template("main-page.html")
 
-@app.route("/update-frames", methods=['POST'])
-def update_frames():
+#defines the job
+def job():
     check = db.execute("SELECT * FROM Frame").fetchall()
     for x in check:
         print(x)
-    return jsonify({"success"})
+        db.execute("SELECT * FROM Package HERE frameID == ?", x).fetchall()
+    #job emits on websocket
+    print(check)
+    socketio.emit('update', check, broadcast=True)
 
-@app.route("/update", method=['POST'])
-def update():
-    return jsonify({"success"})
+#schedule job
+scheduler = BackgroundScheduler()
+running_job = scheduler.add_job(job, 'interval', seconds=1, max_instances=1)
+scheduler.start()
 
 
 def run_server(database):
     global db
     db = database.cursor()
-    app.run()
-    
-    while True:
-        print("Get")
-        check = db.execute("SELECT * FROM Things").fetchall()
-        for x in check:
-            print(x)
-        print(f"---{len(check)}---")
-        sleep(1)
+    socketio.run(app)
